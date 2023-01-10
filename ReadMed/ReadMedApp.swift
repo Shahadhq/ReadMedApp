2bc5f65bed96b4ff2add0a569c75194249856dc6
ShahadQ <shqadi22@twq.idserve.net>
2023-01-10 21:15:24 +0300

    Initial Commit

---
+++ ReadMed/ReadMedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMedApp.swift
+//  ReadMed
+//
+//  Created by ShahadQadi on 17/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ReadMedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
